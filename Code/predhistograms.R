b29b10c8ba8e6f69fbab7d6518b8b2e2fd986e63
Justin Chan <justinchan@Justins-MacBook-Pro-2.local>
2023-11-13 17:32:02 -0500

    fixed predictor histograms figure

--- Code/predhistograms.R
+++ Code/predhistograms.R
@@ -1,7 +1,10 @@
-pdf("predhistograms.pdf")
-library(car)
+pdf("predhistograms.pdf", width = 8.5, height = 5.5)
+
 data1 <- read.csv("all_seasons.csv")
-par(mfrow = c(2,2))
-hist(data1$player_height, data = data1)
-hist(log(data1$player_height), data = data1)
-hist(data1$player_weight, data = data1)
+
+par(mfrow = c(1, 2), mar = c(5, 4, 2, 2) + 0.1)
+
+hist(data1$player_height, main = "Player Height", xlab = "Height(cm)", ylab = "Frequency")
+hist(data1$player_weight, main = "Player Weight", xlab = "Weight(kg)", ylab = "Frequency")
+
+dev.off() 
